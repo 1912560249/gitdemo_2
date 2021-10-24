cf967285358f834f8ace92bb9c38c44b5057471f
1912560249 <1912560249@qq.com>
2021-10-24 14:33:03 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("nice to metting you!");//第一步
+        System.out.println("nice to metting you!");//第一步  我想制作bug
     }
 }
