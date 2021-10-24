9e9880932ab299130d25ddbb97789c755ec601d0
chenyufeng <1912560249@qq.com>
2021-10-24 14:59:15 +0800

    1024

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("nice to metting you!");//换一种方法测试
-        //nice to metting you!
+        System.out.println("nice to metting you!");
+        //第一次 修改 nice to metting you!
     }
 }
