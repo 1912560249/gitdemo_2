e79087bd9c180ac67dba430e2c8829d4fed85514
chenyufeng <1912560249@qq.com>
2021-10-24 14:30:55 +0800

    提交到本地 增加了输入方法 20211024 陈玉峰

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("nice to metting you!");//第一步
     }
 }
