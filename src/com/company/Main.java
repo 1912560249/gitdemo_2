6957d9c1e033db6ee01e0c4b73f3863a19ce11d0
1912560249 <1912560249@qq.com>
2021-10-24 15:27:15 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("nice to metting you!");
-        //第一次 修改 nice to metting you!1
-
-        //第二次 修改 nice to metting you！2
-
-        //第三次 修改 nice to metting you！3
-
-        //第四次 修改 nice to metting you！4
+        //1
+        //2
+        //3
     }
 }
