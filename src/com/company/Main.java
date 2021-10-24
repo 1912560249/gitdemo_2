6601aec536f12dc1d995f8ab0ccae81a1aa7a77a
chenyufeng <1912560249@qq.com>
2021-10-24 15:51:06 +0800

    测试laster

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,8 @@ public class Main {
         //2第二步
         //3第三步
         System.out.println("nice to metting you!");
+        for (int i=0;i<100;i++){
+            System.out.println("打印一个三角形");
+        }
     }
 }
