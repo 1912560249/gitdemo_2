fccd671ebba1e228e427050767f5a77316ad35e3
chenyufeng <1912560249@qq.com>
2021-10-24 16:16:38 +0800

    测试laster

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("nice to metting you!");
-        //1
-        //2
-        //3
-        System.out.println("nice to metting you!");
-        /**
-         * 1
-         * 2
-         * 3
-         * */
+        System.out.println("我是中国人");
+        System.out.println("我是中国人2");
+
     }
 }
