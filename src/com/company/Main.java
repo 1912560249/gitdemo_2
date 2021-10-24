062831504d5d4aa1623a0bfad85ff46ea210e4cd
chenyufeng <1912560249@qq.com>
2021-10-24 16:45:53 +0800

    测试laster

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("nice to metting you!");
+        //1第一步
+        //2第二步
+        //3第三步
+        System.out.println("nice to metting you!");
+        for (int i=0;i<100;i++){
+            System.out.println("打印一个三角形");
+        }
         System.out.println("我是中国人");
         System.out.println("我是中国人2");
-
+        System.out.println("我是中国人3");
     }
 }
---
+++ src/com/company/RTest.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class RTest {
+    private int a;
+    private String b;
+}
